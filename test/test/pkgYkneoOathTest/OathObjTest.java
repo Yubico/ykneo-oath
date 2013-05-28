7576128570252420fcb87ec1760074a5ff2ba5e4
Klas Lindfors <klas@yubico.com>
2013-05-28 10:50:05 +0200

    case 3

--- test/test/pkgYkneoOathTest/OathObjTest.java
+++ test/test/pkgYkneoOathTest/OathObjTest.java
@@ -2,6 +2,8 @@ package pkgYkneoOathTest;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 import pkgYkneoOath.OathObj;
@@ -48,8 +50,9 @@ public class OathObjTest {
 	@Test
 	public void TestSha1Case1() {
 		OathObj obj = new OathObj();
-		obj.setKey(new byte[] {0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b},
-				(short) 0, OathObj.HMAC_SHA1, (short)20);
+		byte[] key = new byte[20];
+		Arrays.fill(key, (byte)0x0b);
+		obj.setKey(key,	(short) 0, OathObj.HMAC_SHA1, (short)20);
 		byte[] res = new byte[20];
 		obj.calculate("Hi There".getBytes(), (short)0, (short) 8, res, (short)0);
 		byte[] expected = new byte[] {(byte) 0xb6, 0x17, 0x31, (byte) 0x86, 0x55, 0x05, 0x72, 0x64, (byte) 0xe2, (byte) 0x8b, (byte) 0xc0, (byte) 0xb6, (byte) 0xfb, 0x37, (byte) 0x8c, (byte) 0x8e, (byte) 0xf1, 0x46, (byte) 0xbe, 0x00};
@@ -63,6 +66,20 @@ public class OathObjTest {
 		byte[] res = new byte[20];
 		obj.calculate("what do ya want for nothing?".getBytes(), (short)0, (short)28, res, (short) 0);
 		byte[] expected = new byte[] {(byte) 0xef, (byte) 0xfc, (byte) 0xdf, 0x6a, (byte) 0xe5, (byte) 0xeb, 0x2f, (byte) 0xa2, (byte) 0xd2, 0x74, 0x16, (byte) 0xd5, (byte) 0xf1, (byte) 0x84, (byte) 0xdf, (byte) 0x9c, 0x25, (byte) 0x9a, 0x7c, 0x79};
+		assertArrayEquals(expected, res);
+	}
+	
+	@Test
+	public void TestSha1Case3() {
+		OathObj obj = new OathObj();
+		byte[] key = new byte[20];
+		Arrays.fill(key, (byte)0xaa);
+		obj.setKey(key, (short) 0, OathObj.HMAC_SHA1, (short)20);
+		byte[] challenge = new byte[50];
+		Arrays.fill(challenge, (byte)0xdd);
+		byte[] res = new byte[20];
+		obj.calculate(challenge, (short)0, (short)50, res, (short)0);
+		byte[] expected = new byte[] {0x12, 0x5d, 0x73, 0x42, (byte) 0xb9, (byte) 0xac, 0x11, (byte) 0xcd, (byte) 0x91, (byte) 0xa3, (byte) 0x9a, (byte) 0xf4, (byte) 0x8a, (byte) 0xa1, 0x7b, 0x4f, 0x63, (byte) 0xf1, 0x75, (byte) 0xd3};
 		assertArrayEquals(expected, res);
 	}
 }
