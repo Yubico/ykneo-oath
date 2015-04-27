22907dc713b92fdf5f1e6aa22a830a69245dbb74
Klas Lindfors <klas@yubico.com>
2015-04-27 15:26:11 +0200

    mark next version as 0.3.0

--- applet/src/pkgYkneoOath/YkneoOath.java
+++ applet/src/pkgYkneoOath/YkneoOath.java
@@ -1,7 +1,7 @@
 package pkgYkneoOath;
 
 /*
- * Copyright (c) 2013 Yubico AB
+ * Copyright (c) 2013-2015 Yubico AB
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -72,7 +72,7 @@ public class YkneoOath extends Applet {
 
 	private byte[] identity;
 
-	private static final byte[] version = {0x00,0x02,0x02};
+	private static final byte[] version = {0x00,0x03,0x00};
 
 	public YkneoOath() {
 		tempBuf = JCSystem.makeTransientByteArray((short) TMP_BUFSIZE, JCSystem.CLEAR_ON_DESELECT);
